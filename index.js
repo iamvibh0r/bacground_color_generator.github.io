4bde630d08e2c16c40bd3de4d7a09a39c334ab3f
Vibhor Sheshrao Patil <121420092+iamvibh0r@users.noreply.github.com>
2023-01-19 15:22:01 +0530

    Update index.js
    Added Some Js

--- index.js
+++ index.js
@@ -13,3 +13,36 @@ function setGradient() {
 
 color1.addEventListener("input",setGradient);
 color2.addEventListener("input",setGradient);
+
+
+
+
+document.addEventListener("contextmenu",function(e){
+  alert("Code Available on github");
+  e.preventDefault();
+});
+
+document.onkeydown = function(e){
+  if(event.keyCode == 123){
+    alert("Code Available on github");
+    return false;
+  }
+
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+  }
+  if(e.ctrlKey && e.keyCode == "U".charCodeAt(0)){
+    alert("Code Available on github");
+    return false;
+    
+  }
+};
